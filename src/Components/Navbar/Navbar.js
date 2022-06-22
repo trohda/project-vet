80c808094e08453825dcc72bb5cd8e48fb2f78b7
Tomasz Rohda <tomaszrohda@wp.pl>
2022-06-22 18:01:21 +0200

    Mobile hamburger menu done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,19 @@
+import { useState } from "react";
 import "../../Styles/Navbar/Navbar.css";
 
 const Navbar = () => {
+  const [icon, setIcon] = useState("navToggler");
+  const [mobileMenuActive, seMobileMenuActive] = useState("");
+
+  const navToggle = () => {
+    if (mobileMenuActive === "") {
+      seMobileMenuActive("activeMobileMenu");
+    } else seMobileMenuActive("");
+
+    if (icon === "navToggler") {
+      setIcon("navToggler toggle");
+    } else setIcon("navToggler");
+  };
   return (
     <navbar>
       <div className="navMenu">
@@ -8,10 +21,21 @@ const Navbar = () => {
         <a href="/onas">O nas</a>
         <a href="/kontakt">Kontakt</a>
       </div>
-      <div className="navToggler">
-        <div></div>
-        <div></div>
-        <div></div>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+      <div className="navMobileMenu">
+        <a href="/home" className={`homeLink ${mobileMenuActive}`}>
+          Strona Główna
+        </a>
+        <a href="/about" className={`aboutLink ${mobileMenuActive}`}>
+          O nas
+        </a>
+        <a href="/contact" className={`contactLink ${mobileMenuActive}`}>
+          Kontakt
+        </a>
       </div>
     </navbar>
   );
