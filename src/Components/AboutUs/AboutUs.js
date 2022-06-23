692149ef92b10d361900c8e764db442abce88f1b
Tomasz Rohda <tomaszrohda@wp.pl>
2022-06-23 21:22:39 +0200

    About us text add

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -1,10 +1,13 @@
 import "../../Styles/AboutUs/AboutUs.css";
-import constr from "../../Img/Icons/constructIcon.svg";
+
 const AboutUs = () => {
   return (
     <section className="aboutUsWrapper">
-      <h2>Podstrona w budowie</h2>
-      <img src={constr} alt=" Construct icon" />
+      <h2>
+        Przychodnia Weterynaryjna <br /> <span> PRZYMORZE</span> <br />{" "}
+        Katarzyna Grodź.
+      </h2>
+      <p>Doświadczenie w połączeniu z empatią i miłością do zwierząt.</p>
     </section>
   );
 };
