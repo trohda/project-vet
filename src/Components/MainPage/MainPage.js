05667fdca8ef4649649205b2fcbfe564eeed9e57
Tomasz Rohda <tomaszrohda@wp.pl>
2022-06-24 11:59:23 +0200

    Main page background and text added

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,12 +1,13 @@
 import "../../Styles/MainPage/MainPage.css";
 
-import Dna from "../Atoms/Dna";
 const MainPage = () => {
   return (
     <div className="mainPage">
       <h1>Weterynarz Przymorze - Kasia Grodź</h1>
 
       <p>Witamy i zapraszamy do gabinetu</p>
+      <p>Od poniedziałku do piątku: 11:00 - 20:00 </p>
+      <p>Sobota: 10:00 - 15:00 </p>
     </div>
   );
 };
