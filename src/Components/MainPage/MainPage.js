012d865fdf5e2561b0478e0326cc693b0af2aee5
Tomasz Rohda <tomaszrohda@wp.pl>
2022-06-29 20:17:33 +0200

    Main page header add

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -3,12 +3,19 @@ import lab from "../../Img/Pics/Lab.png";
 
 const MainPage = () => {
   return (
-    <main>
+    <main className="mainPageWrapper">
+      <div className="mainPageHeader">
+        <h1>
+          Przychodnia Weterynaryjna <br /> <span> PRZYMORZE</span> <br />{" "}
+          Katarzyna Grodź.
+        </h1>
+        <p>Doświadczenie w połączeniu z empatią i miłością do zwierząt.</p>
+      </div>
       <div className="mainPageContent">
-        <p>Witamy i zapraszamy do gabinetu</p>
-        <p>Od poniedziałku do piątku: 11:00 - 20:00 </p>
+        <p>Witamy i zapraszamy do gabinetu.</p>
+        <p>Od poniedziałku do piątku: 11:00 - 20:00</p>
         <p>Sobota: 10:00 - 14:00 </p>
-        <h1>Weterynarz Przymorze - Kasia Grodź</h1>
+
         <img src={lab} alt="Labrador" />
       </div>
     </main>
--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -1,15 +1,7 @@
 import "../../Styles/Team/Team.css";
 
 const Team = () => {
-  return (
-    <section className="aboutUsWrapper">
-      <h2>
-        Przychodnia Weterynaryjna <br /> <span> PRZYMORZE</span> <br />{" "}
-        Katarzyna Grodź.
-      </h2>
-      <p>Doświadczenie w połączeniu z empatią i miłością do zwierząt.</p>
-    </section>
-  );
+  return <section className="teamWrapper"></section>;
 };
 
 export default Team;
