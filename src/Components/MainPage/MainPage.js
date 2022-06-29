e8c78c7375c24c47297c7624539777b9f57e5711
Tomasz Rohda <tomaszrohda@wp.pl>
2022-06-29 19:33:17 +0200

    Add dog pic to main page

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,12 +1,16 @@
 import "../../Styles/MainPage/MainPage.css";
+import lab from "../../Img/Pics/Lab.png";
 
 const MainPage = () => {
   return (
-    <main className="mainPage">
-      <p>Witamy i zapraszamy do gabinetu</p>
-      <p>Od poniedziałku do piątku: 11:00 - 20:00 </p>
-      <p>Sobota: 10:00 - 14:00 </p>
-      <h1>Weterynarz Przymorze - Kasia Grodź</h1>
+    <main>
+      <div className="mainPageContent">
+        <p>Witamy i zapraszamy do gabinetu</p>
+        <p>Od poniedziałku do piątku: 11:00 - 20:00 </p>
+        <p>Sobota: 10:00 - 14:00 </p>
+        <h1>Weterynarz Przymorze - Kasia Grodź</h1>
+        <img src={lab} alt="Labrador" />
+      </div>
     </main>
   );
 };
