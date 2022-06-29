d562e89be0f525dad7b584edb52a6597af17ffcb
Tomasz Rohda <tomaszrohda@wp.pl>
2022-06-29 14:21:13 +0200

    Add offers layout

---
+++ src/Components/Offers/Offer/Offer.js
@@ -0,0 +1,6 @@
+import "../../../Styles/Offers/Offer/Offer.css";
+const Offer = () => {
+  return <article className="offerWrapper"></article>;
+};
+
+export default Offer;
--- src/Components/Offers/Offers.js
+++ src/Components/Offers/Offers.js
@@ -1,6 +1,20 @@
 import "../../Styles/Offers/Offers.css";
+import Offer from "./Offer/Offer";
 const Offers = () => {
-  return <section className="offer"></section>;
+  return (
+    <section className="offersWrapper">
+      <div className="offers">
+        <Offer />
+        <Offer />
+        <Offer />
+        <Offer />
+        <Offer />
+        <Offer />
+        <Offer />
+        <Offer />
+      </div>
+    </section>
+  );
 };
 
 export default Offers;
